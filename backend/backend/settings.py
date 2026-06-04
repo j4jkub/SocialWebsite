ab22031895a186644f7361206b0d39b21292be0f
Jakub Pączko <jakub.paczko01@gmail.com>
2026-06-04 13:52:48 +0200

    backend setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "posts",
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

4a98f04d9ab6670d50da4a9f5b8ca5670d06cebb
Jakub Pączko <jakub.paczko01@gmail.com>
2026-06-06 23:16:34 +0200

    fixes + added media url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
 from posts.views import PostViewSet, CommentViewSet
-from user.views import UserProfileSerializer
+from user.views import UserProfileViewSet
 from rest_framework import routers
+from django.conf.urls.static import static
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
-router.register(r'user', UserProfileSerializer, basename='UserProfile')
+router.register(r'user', UserProfileViewSet, basename='UserProfile')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -6,12 +6,13 @@ from django.contrib.auth.models import User
 
 #TODO add follow count to serializer
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['username']
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
     class Meta:
         model = UserProfile
         fields = ['user', 'bio', 'profile_picture']

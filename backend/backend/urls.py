26e6453c72bc451491ef94f2db2706915d5efe9a
Jakub Pączko <jakub.paczko01@gmail.com>
2026-06-06 22:54:37 +0200

    added user profile serializer

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from posts.views import PostViewSet, CommentViewSet
+from user.views import UserProfileSerializer
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
+router.register(r'user', UserProfileSerializer)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django_filters import rest_framework as django_filters
+from .models import UserProfile
+from .serializers import UserProfileSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer

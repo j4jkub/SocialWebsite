c4be7fcafc83a9777ee367c48a0b23bd03d7183d
Jakub Pączko <jakub.paczko01@gmail.com>
2026-06-15 20:38:42 +0200

    Update views.py

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django_filters import rest_framework as django_filters
-from .filters import CommentFilter
+from .filters import CommentFilter, PostFilter
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.views import APIView
 
 # Create your views here.
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
+    filter_backends = (django_filters.DjangoFilterBackend,)
+    filterset_class = PostFilter
     serializer_class = PostSerializer
 
 class CommentViewSet(viewsets.ModelViewSet):
